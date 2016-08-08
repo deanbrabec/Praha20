096ffdc3fff5a45f6d4db34036ae70f61b72e06b
Jindra Kinský <jindrich.kinsky@ys.cz>
2016-08-08 13:03:37 +0200

    Single action template

---
+++ wp-content/themes/uniqmag-premium-theme-child/single-action.php
@@ -0,0 +1,10 @@
+<?php 
+    if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+    get_header();
+
+    $post_type = get_post_type();
+    $sidebarPosition = Different_Themes()->options->get ( THEME_NAME."_sidebar_position" ); 
+    $sidebarPositionCustom = get_post_meta ( $post->ID, THEME_NAME."_sidebar_position", true ); 
+
+    get_template_part(UNIQMAG_DIFFERENT_THEME_INCLUDES.'news','single');
+    get_footer();
