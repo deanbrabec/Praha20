78adcc036f4ae3eadf5907a87821936a73779f21
Jindra Kinský <jindrich.kinsky@ys.cz>
2016-08-08 10:19:08 +0200

    Post lists layout changes for homepage/category

--- wp-content/themes/uniqmag-premium-theme-child/includes/home-blocks/latest-news-1.php
+++ wp-content/themes/uniqmag-premium-theme-child/includes/home-blocks/latest-news-1.php
@@ -13,8 +13,8 @@
 
 <div class="cs-row">
     <?php if($title) { ?>
-        <div class="cs-col cs-col-6-of-12">
-            <!-- Post block title -->
+        <div class="cs-col cs-col-12-of-12">
+            <!-- Post block title Home -->
             <div class="cs-post-block-title" style="border-left-color: #<?php echo esc_attr($color);?>;">
                 <h4>
                     <?php if($link) { ?>
