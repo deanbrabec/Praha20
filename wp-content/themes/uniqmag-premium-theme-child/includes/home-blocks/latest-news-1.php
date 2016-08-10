0f8c074f6b63978c36168c30c224e38b6e57cfed
Jindra Kinský <jindrich.kinsky@ys.cz>
2016-08-10 13:55:14 +0200

    Control commit (testing)

--- wp-content/themes/uniqmag-premium-theme-child/includes/home-blocks/latest-news-1.php
+++ wp-content/themes/uniqmag-premium-theme-child/includes/home-blocks/latest-news-1.php
@@ -55,14 +55,12 @@
                 <div class="cs-post-block-layout-3">
                     <!-- Post item -->
                     <div class="cs-post-item<?php echo esc_attr($item_class);?>">
-                    
                         <?php if(get_post_type(get_the_ID()) === 'workingplaces') : ?>
                              <?php if(get_field('spolecnost', get_the_ID())) : ?>
                                 <?php the_field('spolecnost', get_the_ID()); ?>
                             <?php endif; ?>
                         <?php endif; ?>
-                        
-
+                       
                         <?php if( $df_post->is_image() ) { ?>
                             <div class="cs-post-thumb">
                                 <?php if( $df_post->compare( get_the_ID(), 'post_category' ) == "1" && $categories ) { ?>
@@ -97,13 +95,6 @@
                                     <?php the_title();?>
                                 </a>
                             </h3>
-<<<<<<< HEAD
-                            
-                                                      
-                            
-=======
-
->>>>>>> origin/master
                             <div class="cs-post-meta cs-clearfix">
                                 <?php if( $df_post->compare( get_the_ID(), 'post_date' ) == "1" ) { ?>
                                     <span class="cs-post-meta-date">
